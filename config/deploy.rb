f22afb18edf24b8af5f42c135f413eaa3a635fc0
jh <jh@jhs-MacBook-Pro.local>
2014-08-26 22:13:46 -0400

    configuring capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'next-mission'
+set :application, 'NextMission'
 set :repo_url, 'git@github.com:jakemh/oli.git'
 set :deploy_to, '/home/nm/www/'
 set :deploy_via, :remote_cache
