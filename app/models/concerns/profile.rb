e4be96c13ad60ebaffe802eefcb9b5e6ebc121bf
jh <jh@jhs-MacBook-Pro.local>
2014-10-01 16:10:23 -0700

    fixed following

--- app/models/concerns/profile.rb
+++ app/models/concerns/profile.rb
@@ -1,26 +1,32 @@
 module Profile
   extend ActiveSupport::Concern
-    included do
-     # field: profile_photo_id, type: Integer
-     # before_create: :notify_on_create
-     # scope: my_scope, ->(var) { where(slug: var) }
+  included do
+    # field: profile_photo_id, type: Integer
+    # before_create: :notify_on_create
+    # scope: my_scope, ->(var) { where(slug: var) }
     has_many :photos, :as => :imageable, :dependent => :destroy
 
-   end
+  end
 
-   def profile_photo
-     if self.profile_photo_id
-       self.photos.find(self.profile_photo_id)
-     end
-   end
+  def profile_photo
+    if self.profile_photo_id
+      self.photos.find_by_id(self.profile_photo_id)
+    end
+  end
 
-   def thumb
-     if profile_photo
-       profile_photo.image.url(:square)
-     end
-   end
+  def small_thumb
+    if profile_photo
+      profile_photo.image.url(:thumb)
+    end
+  end
+
+  def thumb
+    if profile_photo
+      profile_photo.image.url(:square)
+    end
+  end
   # def self.included(base)
-  #   base.field :profile_photo_id, :type => Integer                     
+  #   base.field :profile_photo_id, :type => Integer
   # end
 
 end
