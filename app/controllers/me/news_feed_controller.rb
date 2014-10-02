d06a288b08553929ec8d1bc3b0f4f556f8255070
jh <jh@jhs-MacBook-Pro.local>
2014-10-01 17:26:53 -0700

    added default images

--- app/controllers/me/news_feed_controller.rb
+++ app/controllers/me/news_feed_controller.rb
@@ -7,6 +7,6 @@ class Me::NewsFeedController < MeController
     # @posts = current_user.posts.select post_attrs
     @posts = Post.all.select(post_attrs).sort_by{|p| p.created_at }.reverse
     @post = Post.new
-    @select = [["User Account", ["User", current_user.id, current_user.thumb]]] + current_user.businesses.collect{|b|[b.name,["Business", b.id, b.thumb]]}
+    @select = [["User Account", ["User", current_user.id, current_user.thumb || "null"]]] + current_user.businesses.collect{|b|[b.name,["Business", b.id, b.thumb || "null"]]}
   end
 end
