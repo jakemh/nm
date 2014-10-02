08e41775e2573536993a14b634ba1a6cf232faaf
jh <jh@jhs-MacBook-Pro.local>
2014-10-01 18:05:28 -0700

    ownership no longer subclasses buscon

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,4 +1,6 @@
-class Ownership < BusinessConnection
+class Ownership < Connection
+  validates :connect_to_id, :uniqueness => {:scope => [:user_id]}
+
   # belongs_to :user
   # belongs_to :business
 
