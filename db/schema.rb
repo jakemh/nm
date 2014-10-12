40c5b49bd98770185cf099d134e9bc1f5e7975c7
jh <jh@jhs-MacBook-Pro.local>
2014-10-11 17:17:34 -0700

    added ahoy migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011200132) do
+ActiveRecord::Schema.define(version: 20141012001510) do
 
   create_table "ahoy_events", force: true do |t|
     t.uuid     "visit_id"
