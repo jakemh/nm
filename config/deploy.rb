0ba3783c2eacc80e927162a862a852d3441a5450
Jake Hurley <jakemh@Daniel-McFarlands-MacBook-Pro-3.local>
2014-11-08 19:36:09 -0800

    updated layout AND modified puma hot restarts

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,7 +80,8 @@ namespace :deploy do
     on roles(:web) do
       within current_path do
           puts "RESTARTING PUMA"
-          execute "kill -s USR2 `cat #{puma_pid}`"
+          # execute "kill -s USR2 `cat #{puma_pid}`"
+          execute :bundle, "pumactl -P #{puma_pid} restart"
         end
     end
   end
