fea6d07c78918ad29a9a77b74e743e1769686185
Jake Hurley <jakemh@gmail.com>
2014-11-28 20:28:05 -0800

    modified query to fix postgres error

--- app/controllers/sent_messages_controller.rb
+++ app/controllers/sent_messages_controller.rb
@@ -2,7 +2,7 @@ class SentMessagesController < MessagesController
 
   def index
     # render json: entity.sent_messages.from_entity(@from_entity)
-    render json: entity.sent_messages.joins(:message_recipients).where('message_recipients.? == ?', Post.id_sym(@from_entity.class.name), @from_entity.id)
+    render json: entity.sent_messages.joins(:message_recipients).where('"message_recipients".?=?', Post.id_sym(@from_entity.class.name), @from_entity.id)
 
   end
 
