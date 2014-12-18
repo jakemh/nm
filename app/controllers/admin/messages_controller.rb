21bdf3edebe1a46bbfb7d163cc41b077527d5606
Jake Hurley <jakemh@gmail.com>
2014-12-17 22:00:50 -0800

    fixed admin message delete

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -5,4 +5,15 @@ class Admin::MessagesController < Admin::AdminController
 
   def show
   end
+
+  def destroy
+    @message = Message.find(params[:id])
+    if @message.destroy
+        flash[:notice] = "Message was deleted"
+        redirect_to_back # This redirects to the show action, where the flash will be displayed
+      else
+        flash[:error] = "Something went wrong. Please tell Justin!"
+        redirect_to_back
+    end
+  end
 end
