e11fbe5cfaf3ab9eb751b7a1cd29de8d457eec79
Jake Hurley <jakemh@gmail.com>
2014-12-23 07:40:36 -0500

    removed HTTP authentication

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,7 @@
 class Admin::PostsController <  Admin::AdminController
 
   def index
+    @model = model_type
     @posts = Post.all.includes(:responses, :user, :business).where(type: [nil, "", "Post"]).sort_by{|e| e.id }.reverse
   end
 
