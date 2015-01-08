0919174ab8251969401f4d9a39e363c3e95572ca
Jake Hurley <jakemh@gmail.com>
2015-01-07 19:25:01 -0500

    switched capistrano to beta.nextmission

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-BETA = false
+BETA = true
 
 if BETA 
   role :app, 'beta.nextmission.com'
