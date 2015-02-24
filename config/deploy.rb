e2cb5726caf267523dd235ad1bff425a2c13e157
jake <jake@jakes-MacBook-Pro-3.local>
2015-02-24 13:22:07 -0800

    disabled rvm and enabled rbenv in capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,11 @@ set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{deploy_to}/log/puma_access.log"
 set :puma_error_log, "#{deploy_to}/log/puma_error.log"
 
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
 # root = '/home/jake/repos/nextmission-core/shared'
 
